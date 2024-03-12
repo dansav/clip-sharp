cf4dd7d650c87acedb9a07f54574ff5a850bcf71
Daniel S <dansav@users.noreply.github.com>
2024-03-12 21:53:40 +0100

    fix build

--- ClipSharp/VisualModel.cs
+++ ClipSharp/VisualModel.cs
@@ -1,5 +1,8 @@
 using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace ClipSharp;
 
